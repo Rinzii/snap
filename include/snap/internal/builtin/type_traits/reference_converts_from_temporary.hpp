e54112eb61c669ce2ee2965b8f5c6ddf3f4ff593
ianpike <ianpike98@gmail.com>
2025-12-21 22:42:09 -0500

    Add starting implementation of ref convert from temp

---
+++ include/snap/internal/builtin/type_traits/reference_converts_from_temporary.hpp
@@ -0,0 +1,117 @@
+#pragma once
+
+#include "snap/internal/abi_namespace.hpp"
+
+#include <type_traits>
+
+#if !defined(INTERNAL_SNAP_HAS_BUILTIN_REFERENCE_CONVERTS_FROM_TEMPORARY)
+	#if defined(__NVCOMPILER) || defined(__NVCOMPILER_LLVM__)
+		#define INTERNAL_SNAP_HAS_BUILTIN_REFERENCE_CONVERTS_FROM_TEMPORARY 0
+	#elif defined(__clang__) && (__clang_major__ > 19 || (__clang_major__ == 19 && __clang_minor__ >= 1))
+		#define INTERNAL_SNAP_HAS_BUILTIN_REFERENCE_CONVERTS_FROM_TEMPORARY 1
+	#elif defined(__INTEL_LLVM_COMPILER) && (__INTEL_LLVM_COMPILER >= 20250000)
+		#define INTERNAL_SNAP_HAS_BUILTIN_REFERENCE_CONVERTS_FROM_TEMPORARY 1
+	#elif defined(__GNUC__) && (__GNUC__ >= 13) && !defined(__clang__) && !defined(__CUDACC__)
+		#define INTERNAL_SNAP_HAS_BUILTIN_REFERENCE_CONVERTS_FROM_TEMPORARY 1
+	#else
+		#define INTERNAL_SNAP_HAS_BUILTIN_REFERENCE_CONVERTS_FROM_TEMPORARY 0
+	#endif
+#endif
+
+#if !defined(INTERNAL_SNAP_HAS_BUILTIN_REFERENCE_BINDS_TO_TEMPORARY)
+	#if defined(__NVCOMPILER) || defined(__NVCOMPILER_LLVM__)
+		#define INTERNAL_SNAP_HAS_BUILTIN_REFERENCE_BINDS_TO_TEMPORARY 0
+	#elif defined(__clang__) && (__clang_major__ >= 7) && !defined(__CUDACC__)
+		#define INTERNAL_SNAP_HAS_BUILTIN_REFERENCE_BINDS_TO_TEMPORARY 1
+	#elif defined(__INTEL_LLVM_COMPILER) && (__INTEL_LLVM_COMPILER >= 20210200)
+		#define INTERNAL_SNAP_HAS_BUILTIN_REFERENCE_BINDS_TO_TEMPORARY 1
+	#else
+		#define INTERNAL_SNAP_HAS_BUILTIN_REFERENCE_BINDS_TO_TEMPORARY 0
+	#endif
+#endif
+
+#define INTERNAL_SNAP_HAS_ANY_REFERENCE_TEMPORARY_BUILTIN \
+	(INTERNAL_SNAP_HAS_BUILTIN_REFERENCE_CONVERTS_FROM_TEMPORARY || INTERNAL_SNAP_HAS_BUILTIN_REFERENCE_BINDS_TO_TEMPORARY)
+
+SNAP_BEGIN_NAMESPACE
+namespace builtin
+{
+	template <class U, class = void>
+	struct adjust_ref_binding_source_impl
+	{
+		using type = U;
+	};
+
+	template <class U>
+	struct adjust_ref_binding_source_impl<U, std::enable_if_t<!std::is_reference_v<U> && !std::is_function_v<U>>>
+	{
+		using type = std::remove_cv_t<U>;
+	};
+
+	template <class U>
+	struct adjust_ref_binding_source_impl<U, std::enable_if_t<std::is_function_v<U> && std::is_same_v<void, std::void_t<U&>>>>
+	{
+		using type = U&;
+	};
+
+	template <class U>
+	struct adjust_ref_binding_source
+	{
+		using type = typename adjust_ref_binding_source_impl<U>::type;
+	};
+
+	template <class U>
+	using adjust_ref_binding_source_t = typename adjust_ref_binding_source<U>::type;
+
+	inline constexpr bool has_reference_converts_from_temporary =
+#if INTERNAL_SNAP_HAS_BUILTIN_REFERENCE_CONVERTS_FROM_TEMPORARY
+		true
+#else
+		false
+#endif
+		;
+
+	inline constexpr bool has_reference_binds_to_temporary =
+#if INTERNAL_SNAP_HAS_BUILTIN_REFERENCE_BINDS_TO_TEMPORARY
+		true
+#else
+		false
+#endif
+		;
+
+	inline constexpr bool has_any_reference_temporary_builtin =
+#if INTERNAL_SNAP_HAS_ANY_REFERENCE_TEMPORARY_BUILTIN
+		true
+#else
+		false
+#endif
+		;
+
+	template <class T, class U, class Enable = void>
+	struct reference_converts_from_temporary_impl : std::false_type
+	{
+	};
+
+#if INTERNAL_SNAP_HAS_ANY_REFERENCE_TEMPORARY_BUILTIN
+	template <class T, class U>
+	struct reference_converts_from_temporary_impl<T, U, std::enable_if_t<std::is_reference_v<T>>>
+		: std::bool_constant<
+	#if INTERNAL_SNAP_HAS_BUILTIN_REFERENCE_CONVERTS_FROM_TEMPORARY
+			  __reference_converts_from_temporary(T, adjust_ref_binding_source_t<U>)
+	#else
+			  __reference_binds_to_temporary(T, adjust_ref_binding_source_t<U>)
+	#endif
+		  >
+	{
+	};
+#endif // INTERNAL_SNAP_HAS_ANY_REFERENCE_TEMPORARY_BUILTIN
+
+	template <class T, class U>
+	inline constexpr bool reference_converts_from_temporary_v = reference_converts_from_temporary_impl<T, U>::value;
+
+} // namespace builtin
+SNAP_END_NAMESPACE
+
+#undef INTERNAL_SNAP_HAS_ANY_REFERENCE_TEMPORARY_BUILTIN
+#undef INTERNAL_SNAP_HAS_BUILTIN_REFERENCE_BINDS_TO_TEMPORARY
+#undef INTERNAL_SNAP_HAS_BUILTIN_REFERENCE_CONVERTS_FROM_TEMPORARY
---
+++ include/snap/type_traits/reference_converts_from_temporary.hpp
@@ -0,0 +1,163 @@
+#pragma once
+
+#include "snap/internal/abi_namespace.hpp"
+#include "snap/internal/builtin/type_traits/reference_converts_from_temporary.hpp"
+
+#include <type_traits>
+
+SNAP_BEGIN_NAMESPACE
+
+namespace internal
+{
+	template <class To, class From>
+	inline constexpr bool ref_compatible_v =
+		std::is_object_v<To> && std::is_object_v<From>
+			? std::is_convertible_v<std::add_pointer_t<From>, std::add_pointer_t<To>>
+			: std::is_same_v<To, From>;
+
+	template <class T, class V>
+	inline constexpr bool direct_ref_bind_no_new_temp_v = [] {
+		if constexpr (!std::is_reference_v<T> || !std::is_reference_v<V>)
+		{
+			return false;
+		}
+		else
+		{
+			using TR = std::remove_reference_t<T>;
+			using VR = std::remove_reference_t<V>;
+
+			if constexpr (std::is_lvalue_reference_v<T>)
+			{
+				if constexpr (std::is_lvalue_reference_v<V>)
+				{
+					return ref_compatible_v<TR, VR>;
+				}
+				else
+				{
+					return (std::is_const_v<TR> && !std::is_volatile_v<TR>) && ref_compatible_v<TR, VR>;
+				}
+			}
+			else
+			{
+				if constexpr (std::is_rvalue_reference_v<V>)
+				{
+					return ref_compatible_v<TR, VR>;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+	}();
+
+	template <class T, class V>
+	inline constexpr bool convertible_via_reference_no_new_temp_v = [] {
+		if constexpr (!std::is_reference_v<T>)
+		{
+			return false;
+		}
+		else
+		{
+			using TR = std::remove_reference_t<T>;
+
+			if constexpr (std::is_rvalue_reference_v<T>)
+			{
+				return std::is_convertible_v<V, std::add_rvalue_reference_t<TR>>;
+			}
+			else
+			{
+				return std::is_convertible_v<V, std::add_lvalue_reference_t<TR>>;
+			}
+		}
+	}();
+
+	template <class T, class V>
+	inline constexpr bool scalar_conversion_must_create_temp_v = [] {
+		if constexpr (!std::is_reference_v<T>)
+		{
+			return false;
+		}
+		else
+		{
+			using TR = std::remove_reference_t<T>;
+			using VR = std::remove_reference_t<V>;
+			using TR0 = std::remove_cv_t<TR>;
+			using VR0 = std::remove_cv_t<VR>;
+
+			if constexpr (std::is_scalar_v<TR0> && std::is_scalar_v<VR0> && !std::is_same_v<TR0, VR0>)
+			{
+				return std::is_convertible_v<V, T>;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}();
+
+	template <class T, class U>
+	inline constexpr bool reference_converts_from_temporary_fallback_v = [] {
+		using V = builtin::adjust_ref_binding_source_t<U>;
+
+		if constexpr (!std::is_reference_v<T>)
+		{
+			return false;
+		}
+		else if constexpr (!std::is_convertible_v<V, T>)
+		{
+			return false;
+		}
+		else if constexpr (std::is_lvalue_reference_v<T> && !(std::is_const_v<std::remove_reference_t<T>> && !std::is_volatile_v<std::remove_reference_t<T>>))
+		{
+			return false;
+		}
+		else if constexpr (!std::is_reference_v<U> && !std::is_function_v<U>)
+		{
+			return true;
+		}
+		else if constexpr (direct_ref_bind_no_new_temp_v<T, V>)
+		{
+			return false;
+		}
+		else if constexpr (scalar_conversion_must_create_temp_v<T, V>)
+		{
+			return true;
+		}
+		else if constexpr (convertible_via_reference_no_new_temp_v<T, V>)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}();
+
+	template <class T, class U, bool UseBuiltin = builtin::has_any_reference_temporary_builtin>
+	struct reference_converts_from_temporary_dispatch
+		: std::bool_constant<internal::reference_converts_from_temporary_fallback_v<T, U>>
+	{
+	};
+
+	template <class T, class U>
+	struct reference_converts_from_temporary_dispatch<T, U, true>
+		: std::bool_constant<builtin::reference_converts_from_temporary_v<T, U>>
+	{
+	};
+} // namespace internal
+
+template <class T, class U>
+struct reference_converts_from_temporary
+#if defined(__cpp_lib_reference_from_temporary) && (__cpp_lib_reference_from_temporary >= 202202L)
+	: std::bool_constant<std::reference_converts_from_temporary_v<T, U>>
+#else
+	: std::bool_constant<std::is_reference_v<T> && internal::reference_converts_from_temporary_dispatch<T, U>::value>
+#endif
+{
+};
+
+template <class T, class U>
+inline constexpr bool reference_converts_from_temporary_v = reference_converts_from_temporary<T, U>::value;
+
+SNAP_END_NAMESPACE
