e4b3f534ec90296d45fa2e1fc4e68513987fdebe
ianpike <ianpike98@gmail.com>
2025-12-25 21:45:20 -0500

    Add additional concepts

---
+++ include/snap/concepts.hpp
@@ -0,0 +1,14 @@
+// snap/concepts.hpp
+#pragma once
+
+// Must be included first
+#include "snap/internal/abi_namespace.hpp"
+
+#include "snap/concepts/boolean_testable.hpp"
+#include "snap/concepts/hashable.hpp"
+#include "snap/concepts/istream_extractable.hpp"
+#include "snap/concepts/operators.hpp"
+#include "snap/concepts/ostream_insertable.hpp"
+#include "snap/concepts/pair_like.hpp"
+#include "snap/concepts/tuple_like.hpp"
+#include "snap/concepts/variant_like.hpp"
---
+++ include/snap/concepts/boolean_testable.hpp
@@ -0,0 +1,30 @@
+// snap/concepts/boolean_testable.hpp
+#pragma once
+
+// Must be included first
+#include "snap/internal/abi_namespace.hpp"
+
+#if !defined(__cpp_concepts) || (__cpp_concepts < 201907L)
+	#error "snap/concepts/boolean_testable.hpp requires C++20 concepts."
+#endif
+
+#ifdef __has_include
+	#if !__has_include(<concepts>)
+		#error "snap/concepts/boolean_testable.hpp requires the <concepts> header."
+	#endif
+#endif
+
+#include <concepts>
+#include <utility>
+
+SNAP_BEGIN_NAMESPACE
+
+namespace concepts
+{
+	template <class T>
+	concept boolean_testable = std::convertible_to<T, bool> && requires(T&& t) {
+		{ !std::forward<T>(t) } -> std::convertible_to<bool>;
+	};
+} // namespace concepts
+
+SNAP_END_NAMESPACE
