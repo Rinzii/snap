b1e8cdf522af1eddcc77c18854b068c5061f6a8a
ianpike <ianpike98@gmail.com>
2025-12-25 21:46:32 -0500

    Remove SNAP_HAS_INCLUDE due to issues with lexing

--- include/snap/internal/pp/has_include.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifndef SNAP_HAS_INCLUDE
-	#ifdef __has_include
-		#define SNAP_HAS_INCLUDE(INCLUDE) __has_include(INCLUDE)
-	#else
-		#define SNAP_HAS_INCLUDE(INCLUDE) (0)
-	#endif
-#endif
